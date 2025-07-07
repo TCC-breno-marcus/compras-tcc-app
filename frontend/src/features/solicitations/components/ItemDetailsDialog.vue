0182416c0ed23eabeb1cd4b1c2e4306d1e7bca52
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-07-07 18:56:47 -0300

    feat: item details dialog

--- frontend/src/features/solicitations/components/ItemDetailsDialog.vue
+++ frontend/src/features/solicitations/components/ItemDetailsDialog.vue
@@ -2,6 +2,7 @@
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
 import type { ItemCatalogo } from '@/types/itemsCatalogo';
+import { Divider } from 'primevue';
 
 // 1. Defina as PROPS que este componente vai receber do pai
 defineProps<{
@@ -19,30 +20,43 @@ const closeModal = () => {
 </script>
 
 <template>
-  <Dialog v-if="item" :visible="visible" @update:visible="closeModal" modal :header="item.title"
+  <Dialog v-if="item" :visible="visible" @update:visible="closeModal" modal :header="`Detalhes do Material`"
     :style="{ width: 'clamp(300px, 50vw, 700px)' }">
-    <div class="dialog-content">
-      <img :src="`https://primefaces.org/cdn/primevue/images/galleria/galleria${item.code.slice(-1)}.jpg`"
-        :alt="item.title" class="dialog-image mb-4" />
-      <p>Aqui estão mais informações sobre o item **{{ item.title }}**.</p>
-      <p><strong>Código CATMAT:</strong> {{ item.code }}</p>
-      <p><strong>Descrição:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
-        lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+    <div class="dialog-content flex">
+      <div class="flex flex-column justify-content-between text-justify">
+        <div>
+          <p><strong>Título:</strong> {{ item.title }}</p>
+          <p><strong>CATMAT:</strong> {{ item.code }}</p>
+          <p><strong>Descrição:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+            Suspendisse
+            lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+        </div>
+        <div>
+          <Divider align="left" type="solid">
+            <b>Materiais Semelhantes</b>
+          </Divider>
+          <p>Balão Fundo Chato 2000 mL - CATMAT: 409256</p>
+          <p>Balão Fundo 6000 mL - CATMAT: 413186</p>
+        </div>
+      </div>
+      <div class="dialog-img">
+        <img src="/items_img/img1.png" :alt="item.title" class="dialog-image mb-4" />
+      </div>
     </div>
 
+
     <template #footer>
-      <Button label="Fechar" icon="pi pi-times" @click="closeModal" text />
-      <Button label="Adicionar à Solicitação" icon="pi pi-plus" />
+      <Button label="Fechar" icon="pi pi-times" @click="closeModal" text size="small"/>
+      <Button label="Adicionar à Solicitação" icon="pi pi-plus" size="small"/>
     </template>
   </Dialog>
 </template>
 
 <style scoped>
 .dialog-image {
-  width: 100%;
-  max-height: 300px;
   object-fit: cover;
   border-radius: 8px;
+  max-width: 10rem;
 }
 
 .dialog-content p {
