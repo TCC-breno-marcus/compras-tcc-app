b530b68b7441225331c4d95b4d8c39cb084f5262
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-07-18 21:10:26 -0300

    feat: create custom paginator component

---
+++ frontend/src/components/CustomPaginator.vue
@@ -0,0 +1,165 @@
+<script setup>
+import { computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import Button from 'primevue/button'
+
+const props = defineProps({
+  currentUrl: {
+    type: String,
+    required: true,
+  },
+  totalRecords: {
+    type: Number,
+    required: true,
+  },
+  hasNextPage: {
+    type: Boolean,
+    required: true,
+  },
+})
+
+const router = useRouter()
+const route = useRoute()
+
+const currentParams = computed(() => {
+  const filter = route.query.filter || ''
+  return new URLSearchParams(filter.toString())
+})
+
+const currentPage = computed(() => {
+  return Number(currentParams.value.get('page')) || 1
+})
+
+const MAX_PAGE_SIZE = 50
+
+const totalPages = computed(() => {
+  const qtdPages = props.totalRecords / MAX_PAGE_SIZE
+  return qtdPages % 1 !== 0 ? Math.floor(qtdPages) + 1 : qtdPages
+})
+
+const handleNavigation = (action) => {
+  const actionMap = {
+    first: 1,
+    last: totalPages.value,
+    next: Math.min(currentPage.value + 1, totalPages.value),
+    previous: Math.max(currentPage.value - 1, 1),
+  }
+
+  const page = typeof action === 'string' ? actionMap[action] : action
+
+  const newParams = new URLSearchParams(currentParams.value.toString())
+  newParams.set('page', page.toString())
+
+  router.push({
+    path: props.currentUrl,
+    query: { filter: newParams.toString() },
+  })
+}
+
+const visiblePages = computed(() => {
+  const links = []
+  const maxVisiblePages = 5
+  let startPage = Math.max(1, currentPage.value - Math.floor(maxVisiblePages / 2))
+  let endPage = Math.min(totalPages.value, startPage + maxVisiblePages - 1)
+
+  if (endPage - startPage < maxVisiblePages - 1) {
+    startPage = Math.max(1, endPage - maxVisiblePages + 1)
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    links.push(i)
+  }
+  return links
+})
+</script>
+
+<template>
+  <div class="flex align-items-center gap-1">
+    <Button
+      :pt="{
+        root: {
+          class: 'buttonNavigation'
+        },
+      }"
+      type="button"
+      rounded
+      icon="pi pi-angle-double-left"
+      @click="handleNavigation('first')"
+      :disabled="currentPage === 1"
+      text
+      size="small"
+    />
+    <Button
+      :pt="{
+        root: {
+          class: 'buttonNavigation'
+        },
+      }"
+      type="button"
+      rounded
+      icon="pi pi-angle-left"
+      @click="handleNavigation('previous')"
+      :disabled="currentPage === 1"
+      text
+      size="small"
+    />
+
+    <Button
+      v-for="pageNumber in visiblePages"
+      :key="`page-button-${pageNumber}`"
+      :pt="{
+        root: {
+          class: ['buttonNavigation', { buttonNavigationCurrent: pageNumber === currentPage }],
+        },
+      }"
+      type="button"
+      :label="pageNumber.toString()"
+      rounded
+      @click="handleNavigation(pageNumber)"
+      text
+      size="small"
+    />
+
+    <Button
+      :pt="{
+        root: {
+          class: 'buttonNavigation'
+        },
+      }"
+      type="button"
+      rounded
+      icon="pi pi-angle-right"
+      @click="handleNavigation('next')"
+      :disabled="!props.hasNextPage"
+      text
+      size="small"
+    />
+    <Button
+      :pt="{
+        root: {
+          class: 'buttonNavigation'
+        },
+      }"
+      type="button"
+      rounded
+      icon="pi pi-angle-double-right"
+      @click="handleNavigation('last')"
+      :disabled="!props.hasNextPage"
+      text
+      size="small"
+    />
+  </div>
+</template>
+
+<style scoped>
+.buttonNavigation {
+  color: var(--p-surface-400) !important;
+  width: 40px !important;
+  height: 40px !important;
+}
+
+.buttonNavigationCurrent {
+  background-color: var(--p-button-text-primary-hover-background) !important;
+  color: var(--p-surface-500) !important;
+}
+</style>
--- frontend/src/features/management/components/ManageItems.vue
+++ frontend/src/features/management/components/ManageItems.vue
@@ -1,233 +1,241 @@
 <script setup lang="ts">
-import CatalogUpload from "./CatalogUpload.vue"
-import { ref } from 'vue';
-import Toolbar from 'primevue/toolbar';
-import IconField from 'primevue/iconfield';
-import InputIcon from 'primevue/inputicon';
-import SplitButton from 'primevue/splitbutton';
-import InputText from 'primevue/inputtext';
-import Select from 'primevue/select';
-import ItemComponent from './ItemComponent.vue';
-import ItemDetailsDialog from './ItemDetailsDialog.vue';
-import type { ItemCatalogo } from '@/types/itemsCatalogo';
-import { Button } from 'primevue';
-import Paginator from 'primevue/paginator';
+import CatalogUpload from './CatalogUpload.vue'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import IconField from 'primevue/iconfield'
+import InputIcon from 'primevue/inputicon'
+import SplitButton from 'primevue/splitbutton'
+import InputText from 'primevue/inputtext'
+import Select from 'primevue/select'
+import ItemComponent from './ItemComponent.vue'
+import ItemDetailsDialog from './ItemDetailsDialog.vue'
+import type { ItemCatalogo } from '@/types/itemsCatalogo'
+import { Button } from 'primevue'
+import CustomPaginator from '@/components/CustomPaginator.vue'
 
+const route = useRoute()
 
 const filters = ref([
   {
     label: 'Ordenar por',
-    disabled: true
+    disabled: true,
   },
   {
     label: 'Mais Pedidos',
-    icon: 'pi pi-arrow-up'
+    icon: 'pi pi-arrow-up',
   },
   {
     label: 'Menos Pedidos',
-    icon: 'pi pi-arrow-down'
-  }
-]);
+    icon: 'pi pi-arrow-down',
+  },
+])
 
-const selectedCategory = ref();
+const selectedCategory = ref()
 
 const categories = ref([
   { name: 'Todas', code: 'todas' },
   { name: 'Vidraria', code: 'vidraria' },
   { name: 'Componentes Eletrônicos', code: 'componentes-eletronicos' },
-]);
+])
 
 const items = ref([
   {
     title: 'Seringa Descartável',
     img: '/items_img/img1.png',
     code: '214560',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Gaze Estéril Pacote',
     img: '/items_img/img1.png',
     code: '348790',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Luva Cirúrgica (Par)',
     img: '/items_img/img1.png',
     code: '551230',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Álcool Etílico 70% 1L',
     img: '/items_img/img1.png',
     code: '987010',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Seringa Descartável',
     img: '/items_img/img1.png',
     code: '214560',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Gaze Estéril Pacote',
     img: '/items_img/img1.png',
     code: '348790',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Luva Cirúrgica (Par)',
     img: '/items_img/img1.png',
     code: '551230',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Álcool Etílico 70% 1L',
     img: '/items_img/img1.png',
     code: '987010',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Seringa Descartável',
     img: '/items_img/img1.png',
     code: '214560',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Gaze Estéril Pacote',
     img: '/items_img/img1.png',
     code: '348790',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Luva Cirúrgica (Par)',
     img: '/items_img/img1.png',
     code: '551230',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Álcool Etílico 70% 1L',
     img: '/items_img/img1.png',
     code: '987010',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Seringa Descartável',
     img: '/items_img/img1.png',
     code: '214560',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Gaze Estéril Pacote',
     img: '/items_img/img1.png',
     code: '348790',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Luva Cirúrgica (Par)',
     img: '/items_img/img1.png',
     code: '551230',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Álcool Etílico 70% 1L',
     img: '/items_img/img1.png',
     code: '987010',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Seringa Descartável',
     img: '/items_img/img1.png',
     code: '214560',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Gaze Estéril Pacote',
     img: '/items_img/img1.png',
     code: '348790',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Luva Cirúrgica (Par)',
     img: '/items_img/img1.png',
     code: '551230',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Álcool Etílico 70% 1L',
     img: '/items_img/img1.png',
     code: '987010',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Seringa Descartável',
     img: '/items_img/img1.png',
     code: '214560',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Gaze Estéril Pacote',
     img: '/items_img/img1.png',
     code: '348790',
-    status: 'Ativo'
+    status: 'Ativo',
   },
   {
     title: 'Luva Cirúrgica (Par)',
     img: '/items_img/img1.png',
     code: '551230',
-    status: 'Inativo'
+    status: 'Inativo',
   },
   {
     title: 'Álcool Etílico 70% 1L',
     img: '/items_img/img1.png',
     code: '987010',
-    status: 'Ativo'
-  }
-]);
+    status: 'Ativo',
+  },
+])
 
-const isDialogVisible = ref(false);
-const selectedItem = ref<ItemCatalogo | null>(null);
+const isDialogVisible = ref(false)
+const selectedItem = ref<ItemCatalogo | null>(null)
 const handleViewDetails = (item: ItemCatalogo) => {
-  selectedItem.value = item;
-  isDialogVisible.value = true;
-};
-
+  selectedItem.value = item
+  isDialogVisible.value = true
+}
 </script>
 
 <template>
   <div class="flex flex-column w-full h-full">
- 
     <div class="flex flex-wrap align-items-center justify-content-between gap-2 md:gap-4 mt-2">
-
       <div class="flex flex-wrap align-items-center gap-2">
-
         <div class="flex flex-column sm:flex-row gap-2">
           <IconField iconPosition="left">
             <InputIcon class="pi pi-search"></InputIcon>
             <InputText size="small" placeholder="Nome/Descrição/CATMAT" />
           </IconField>
 
-          <Select v-model="selectedCategory" :options="categories" optionLabel="name" placeholder="Categoria"
-            class="w-full md:w-56" size="small" />
+          <Select
+            v-model="selectedCategory"
+            :options="categories"
+            optionLabel="name"
+            placeholder="Categoria"
+            class="w-full md:w-56"
+            size="small"
+          />
         </div>
 
         <div class="flex align-items-center gap-2">
           <SplitButton text size="small" label="Ordenar por" :model="filters" />
           <Button type="button" label="Filtrar" icon="pi pi-filter" size="small" />
         </div>
-
       </div>
 
-      <CatalogUpload />
-
+      <div class="flex align-items-center gap-2">
+        <Button type="button" label="Criar" icon="pi pi-plus" size="small" />
+        <CatalogUpload />
+      </div>
     </div>
 
     <div class="items-grid mt-2 gap-2">
-      <ItemComponent v-for="item in items" :key="item.code" :item="item" @viewDetails="handleViewDetails" />
-      <Paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]"></Paginator>
+      <ItemComponent
+        v-for="item in items"
+        :key="item.code"
+        :item="item"
+        @viewDetails="handleViewDetails"
+      />
+      <CustomPaginator :current-url="route.path" :total-records="300" :has-next-page="true" />
     </div>
 
     <ItemDetailsDialog v-model:visible="isDialogVisible" :item="selectedItem" />
-
   </div>
 </template>
 
