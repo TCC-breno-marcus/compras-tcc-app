334f8e17ad9871644b3884d71cab33dc138771e4
brenosbarroso <barrososbreno@icloud.com>
2025-07-21 19:39:20 -0300

    feat: implement JWT authentication and refactor environment variable handling in Docker

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,9 @@ using Database;
 using Microsoft.EntityFrameworkCore;
 using Services;
 using Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Authentication setup
+var config = builder.Configuration;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // Regras para validar o token:
+        ValidateIssuer = true, // Validar quem emitiu
+        ValidateAudience = true, // Validar para quem foi emitido
+        ValidateLifetime = true, // Validar se não expirou
+        ValidateIssuerSigningKey = true, // Validar a assinatura (a parte mais importante)
+
+        // Valores válidos (lidos do nosso appsettings.json):
+        ValidIssuer = config["Jwt:Issuer"],
+        ValidAudience = config["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!))
+    };
+});
+
+
+
 // CORS
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
@@ -17,7 +47,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173") 
+                          policy.WithOrigins("http://localhost:5173")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
@@ -43,6 +73,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
