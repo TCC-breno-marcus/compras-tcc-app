63ca43d49d2a373c98f8a0457d72973738d12106
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-08-02 21:18:32 -0300

    fix: resolving warnings

--- backend/src/Models/Entities/Solicitantes/Solicitante.cs
+++ backend/src/Models/Entities/Solicitantes/Solicitante.cs
@@ -13,7 +13,6 @@ namespace ComprasTccApp.Models.Entities.Solicitantes
         [Required]
         public required Servidor Servidor { get; set; }
 
-        [Required]
         public List<Solicitacao> Solicitacoes { get; set; } = new List<Solicitacao>();
 
         [Required]
