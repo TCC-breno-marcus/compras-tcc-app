a6ab56bc64308bc18fe943e087f622415f0bd406
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-09-04 11:49:49 -0300

    fix: bug in show card from home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -16,9 +16,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
     </div>
 
     <div class="grid ml-2">
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isSolicitante" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isSolicitante"
           title="Nova Solicitação"
           title-icon="add_shopping_cart"
           content-text="Inicie uma nova solicitação para itens gerais ou patrimoniais."
@@ -30,9 +29,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
         />
       </div>
 
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isSolicitante" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isSolicitante"
           title="Minhas Solicitações"
           title-icon="assignment_ind "
           content-text="Visualize ou edite todas as suas solicitações."
@@ -42,9 +40,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
         />
       </div>
 
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isGestor" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isGestor"
           title="Itens por Departamento"
           title-icon="segment"
           content-text="Visualize os itens solicitados divididos por departamento."
@@ -54,9 +51,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
         />
       </div>
 
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isGestor" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isGestor"
           title="Solicitações"
           title-icon="checklist"
           content-text="Visualize e revise as solicitações de compra abertas."
@@ -66,9 +62,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
         />
       </div>
 
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isGestor" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isGestor"
           title="Catálogo"
           title-icon="inventory_2"
           content-text="Adicione, edite ou remova itens e controle a disponibilidade deles no sistema."
@@ -78,9 +73,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
         />
       </div>
 
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isGestor" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isGestor"
           title="Dashboards"
           title-icon="bar_chart"
           content-text="Visualize gráficos e indicadores sobre as solicitações de compra."
@@ -90,9 +84,8 @@ const { user, isAdmin, isGestor, isSolicitante } = storeToRefs(authStore)
         />
       </div>
 
-      <div class="col-12 md:col-6 lg:col-4">
+      <div v-if="isAdmin || isGestor" class="col-12 md:col-6 lg:col-4">
         <HomeCard
-          v-if="isAdmin || isGestor"
           title="Relatórios"
           title-icon="article"
           content-text="Gere relatórios detalhados sobre as solicitações de compra."
